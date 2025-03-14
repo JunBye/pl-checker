92954382b1122c1d0d6cc867d7a77fa7d3b2de6b
Junbye <pjb0621@naver.com>
2025-03-15 00:02:01 +0900

    HW2-EX2 TestCase 추가

--- 2025/hw2/test2.ml
+++ 2025/hw2/test2.ml
@@ -39,6 +39,49 @@ module TestEx2: TestEx =
         SEQ [INSERT 1; INSERT 2; INSERT 3; FINDMIN 1; DELETEMIN; FINDMIN 2; DELETEMIN; FINDMIN 3; DELETEMIN;];
         SEQ [INSERT 3; INSERT 2; INSERT 1; FINDMIN 1; DELETEMIN; FINDMIN 2; DELETEMIN; FINDMIN 3; DELETEMIN;];
         SEQ [INSERT 5; INSERT 5; INSERT 5; FINDMIN 5; DELETEMIN; INSERT 4; FINDMIN 4; DELETEMIN; INSERT 1; FINDMIN 1;];
+        SEQ [
+          (* A 힙 노드 삽입 *)
+          INSERT 1; INSERT 10; INSERT 12;
+          INSERT 8; INSERT 11; INSERT 16;
+
+          (* B 힙 노드 삽입 *)
+          INSERT 3; INSERT 4; INSERT 7;
+          INSERT 6; INSERT 20; INSERT 10;
+          INSERT 25; INSERT 21; INSERT 15;
+          INSERT 27;
+
+          (* 간단한 검증: 현재 힙에서 최솟값은 1일 것이라 가정 *)
+          FINDMIN 1;
+          DELETEMIN;
+          (* 1 삭제 후 최솟값이 3이라고 가정 *)
+          FINDMIN 3
+        ];
+        SEQ [
+          (* 왼쪽 힙의 값들 삽입 *)
+          INSERT 1; INSERT 10; INSERT 8; INSERT 12; INSERT 11; INSERT 16;
+          (* 오른쪽 힙의 값들 삽입 *)
+          INSERT 3; INSERT 4; INSERT 7; INSERT 6; INSERT 20; INSERT 10; INSERT 25; INSERT 21; INSERT 15; INSERT 27;
+          (* 전체 힙에 대한 최소값 삭제 순서: 
+            병합된 힙에서 최소값은 오름차순으로: 
+            1, 3, 4, 6, 7, 8, 10, 10, 11, 12, 15, 16, 20, 21, 25, 27 *)
+          FINDMIN 1;  DELETEMIN;  (* 1 삭제 *)
+          FINDMIN 3;  DELETEMIN;  (* 3 삭제 *)
+          FINDMIN 4;  DELETEMIN;  (* 4 삭제 *)
+          FINDMIN 6;  DELETEMIN;  (* 6 삭제 *)
+          FINDMIN 7;  DELETEMIN;  (* 7 삭제 *)
+          FINDMIN 8;  DELETEMIN;  (* 8 삭제 *)
+          FINDMIN 10; DELETEMIN;  (* 첫 번째 10 삭제 *)
+          FINDMIN 10; DELETEMIN;  (* 두 번째 10 삭제 *)
+          FINDMIN 11; DELETEMIN;  (* 11 삭제 *)
+          FINDMIN 12; DELETEMIN;  (* 12 삭제 *)
+          FINDMIN 15; DELETEMIN;  (* 15 삭제 *)
+          FINDMIN 16; DELETEMIN;  (* 16 삭제 *)
+          FINDMIN 20; DELETEMIN;  (* 20 삭제 *)
+          FINDMIN 21; DELETEMIN;  (* 21 삭제 *)
+          FINDMIN 25; DELETEMIN;  (* 25 삭제 *)
+          FINDMIN 27; DELETEMIN;  (* 27 삭제 *)
+          FINDMIN_EMPTY; DELETEMIN_EMPTY; (* 힙이 비어있음을 확인 *)
+        ];
       ]
 
     let runner tc =
